e83150846208b072aaa4fe61490d24029541b2e9
Greg Behnke <gregbehnke24x7@msn.com>
2021-10-18 09:57:00 -0500

    commit progress

--- mission_to_mars/app.py
+++ mission_to_mars/app.py
@@ -12,8 +12,16 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 # Create route that renders index.html template and finds documents from mongo
 @app.route("/")
 def home():
-    # Fetch data
-    mars_data = mongo.db.mars.find_one()
+    # see if collection exists yet
+    mars_data = mongo.db.mars.find()
+    # check to see if any results are returned
+    ret = list(mars_data)
+    # If the collection was not found, do the scrape and upsert
+    if len(ret)==0:
+        mars_data = scrape_mars.scrape()
+    else:
+        mars_data = mongo.db.mars.find_one()
+
     # Return template and data
     return render_template("index.html", mars_data=mars_data)
 
